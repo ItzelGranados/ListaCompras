8b49fe07c646b19bad34845c77c363867f71b309
ItzelGranados <itzelgranados13@gmail.com>
2022-06-02 14:15:28 -0500

    Se agregaron ediciones a Lista de Compras

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+let contador = 0;
+let costoTotal = 0;
+
 let element = document.getElementById("totalPrecio");
 element.innerHTML = "Total en Precio";
 
 let txtNombre = document.getElementById("Name");
+
 //txtNombre.value = "Leche Semidescremada";
 //console.log(txtNombre.value);
 
 let txtNumber = document.getElementById("Number");
+
+let total = document.getElementById("precioTotal");
 
 /*let campos = document.getElementsByClassName("campo");
 campos[0].value = "Leche descremada deslactosada light=Agua";
@@ -35,14 +41,63 @@ let cuerpoTabla = tabla.getElementsByTagName("tbody");
 <td>$ 23.50</td>
 </tr> `;*/
 
+function validarNombre() {
+    if (txtNombre.value.length < 3) {
+        return false;
+    } //if
+    return true;
+} //valida nombre 
+
+function validarCantidad() {
+    if (txtNumber.value.length == 0) {
+        return false;
+    }
+    if (parseFloat(txtNumber.value) <= 0) {
+        return false;
+    } //if
+    return true;
+} //validar cantidad
+
 let agregar = document.getElementById("btnAgregar");
 
 
 agregar.addEventListener("click", (event) => {
-    let precio = Math.random() * 200;
+    event.preventDefault();
+
+    if ((!validarNombre()) || (!validarCantidad())) {
+        document.getElementById("alertValidacionesTexto").innerHTML = "Los campos deben ser llenados correctamente";
+        document.getElementById("alertValidaciones").style.display = "block";
+        if (!validarNombre()) {
+            txtNombre.style.border = "red thin solid";
+        } //
+        if (!validarCantidad()) {
+            txtNumber.style.border = "red thin solid";
+        }
+
+        setTimeout(function() {
+                document.getElementById("alertValidaciones").style.display = "none";
+            },
+            3000
+        );
+
+        return false;
+    }
+
+    txtNumber.style.border = "";
+    txtNombre.style.border = "";
+
+    document.getElementById("alertValidaciones").style.display = "none";
+    contador++;
+    document.getElementById("contadorProductos").innerHTML = contador;
+    let precio = (Math.floor((Math.random() * 50) * 100)) / 100;
+    let cantidad = parseFloat(txtNumber.value);
+    costoTotal += (precio * cantidad);
+    total.innerHTML = ` $ ${costoTotal}`;
+
 
     let tmp = `<tr>
 
+    <th.scope="row">${contador}</th>
     <th scope="row">1</th>
     <td>${txtNombre.value}</td>
     <td>${txtNumber.value}</td>
@@ -58,3 +113,11 @@ agregar.addEventListener("click", (event) => {
     console.log(txtNombre.value, txtNumber.value);
 
 });
+
+txtNombre.addEventListener("blur", (event) => {
+    event.target.value = event.target.value.trim();
+})
+
+txtNumber.addEventListener("blur", (event) => {
+    event.target.value = event.target.value.trim();
+})
